96873df43168d7bdc4f0e0836f1974e18143c1e8
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-05-27 14:38:45 +0100

    Feature: access token is correctly saved

--- src/Pages/Login/__tests__/Login.test.js
+++ src/Pages/Login/__tests__/Login.test.js
@@ -1,62 +1,76 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
-import axios from 'axios';
-import Login from '../index';
+import axios from "axios";
+import Login from "../index";
 
-jest.mock('axios');
+jest.mock("axios");
 
-describe('Login page', () => {
-  it('has an element with the correct header text', () => {
+describe("Login page", () => {
+  it("has an element with the correct header text", () => {
     render(<Login />);
-    const header = screen.getByText('Sign In');
+    const header = screen.getByText("Sign In");
     expect(header).toBeInTheDocument();
-  })
+  });
 
-  it('has a username label & input field', () => {
+  it("has a username label & input field", () => {
     render(<Login />);
-    const usernameLabel = screen.getByLabelText('User Name');
-    const usernameInput = screen.getByTestId('username-input');
+    const usernameLabel = screen.getByLabelText("User Name");
+    const usernameInput = screen.getByTestId("username-input");
     expect(usernameLabel).toBeInTheDocument();
     expect(usernameInput).toBeInTheDocument();
-  })  
+  });
 
-  it('has a password label & input field', () => {
+  it("has a password label & input field", () => {
     render(<Login />);
-    const passwordLabel = screen.getByLabelText('Password');
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordLabel = screen.getByLabelText("Password");
+    const passwordInput = screen.getByTestId("password-input");
     expect(passwordLabel).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
-  })  
+  });
 
-  it('has a sign in button', () => {
+  it("has a sign in button", () => {
     render(<Login />);
-    const submitBtn = screen.getByText('Sign in');
+    const submitBtn = screen.getByText("Sign in");
     expect(submitBtn).toBeInTheDocument();
-  })
+  });
 
-  it('calls axios when the sign in button in clicked', () => {
+  it("calls axios when the sign in button in clicked", () => {
     render(<Login />);
-    userEvent.click(screen.getByText('Sign in'));
-    expect(axios.post).toHaveBeenCalled()
-  })
+    userEvent.click(screen.getByText("Sign in"));
+    expect(axios.post).toHaveBeenCalled();
+  });
 
-  it('calls the API with the correct data', () => {
-    const url = 'https://api.bybits.co.uk/auth/token'
-    const headers = {
-        'environment': 'mock',
-        'Content-Type': 'application/json'
-    }
+  it("calls the API with the correct data", () => {
+    const url = "https://api.bybits.co.uk/auth/token";
+    const config = {
+      headers: {
+        environment: "mock",
+        "Content-Type": "application/json",
+      },
+    };
     const data = {
-      "username":"testuser",
-      "password":"1234",
-      "type":"USER_PASSWORD_AUTH"
-    }
+      username: "testuser",
+      password: "1234",
+      type: "USER_PASSWORD_AUTH",
+    };
     render(<Login />);
-    userEvent.type(screen.getByTestId('username-input'), data.username)
-    userEvent.type(screen.getByTestId('password-input'), data.password)
-    userEvent.click(screen.getByText('Sign in'));
-    expect(axios.post).toHaveBeenCalledWith(url, headers, data);
-  })
-})
+    userEvent.type(screen.getByTestId("username-input"), data.username);
+    userEvent.type(screen.getByTestId("password-input"), data.password);
+    userEvent.click(screen.getByText("Sign in"));
+    expect(axios.post).toHaveBeenCalledWith(url, data, config);
+  });
+
+  it("saves the auth token correctly", async () => {
+    axios.post.mockReturnValue({
+      data: {
+        access_token: "123",
+      },
+    });
+    render(<Login />);
+    userEvent.click(screen.getByText("Sign in"));
+    await waitFor(() => expect(axios.post).toHaveBeenCalled());
+    expect(localStorage.getItem("token")).toBe("123");
+  });
+});
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,35 +1,57 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const url = 'https://api.bybits.co.uk/auth/token'
-  const headers = {
-    'environment': 'mock',
-    'Content-Type': 'application/json'
-  }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "https://api.bybits.co.uk/auth/token";
+  const config = {
+    headers: {
+      environment: "mock",
+      "Content-Type": "application/json",
+    },
+  };
 
-  const handleClick = () => {
+  const handleClick = async () => {
     const data = {
-      username, 
+      username,
       password,
-      "type":"USER_PASSWORD_AUTH"
+      type: "USER_PASSWORD_AUTH",
+    };
+    try {
+      const response = await axios.post(url, data, config);
+      const access_token = response.data.access_token;
+      localStorage.setItem('token', access_token);
+    } catch (error) {
+      console.log(error)
     }
-    axios.post(url, headers, data)
+
   };
 
   return (
     <div>
       <h2>Sign In</h2>
-      <form>
-        <label htmlFor='username'>User Name</label>
-        <input type='text' id='username' data-testid='username-input' onChange={(e) => setUsername(e.target.value)} value={username}/>
-        <label htmlFor='password'>Password</label>
-        <input type='text' id='password' data-testid='password-input'onChange={(e) => setPassword(e.target.value)} value={password}/>
-        <button type='button' onClick={handleClick}>Sign in</button>
-      </form>
-    </div>
 
-  )
+        <label htmlFor="username">User Name</label>
+        <input
+          type="text"
+          id="username"
+          data-testid="username-input"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          data-testid="password-input"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button type="button" onClick={handleClick}>
+          Sign in
+        </button>
+
+    </div>
+  );
 }
