ef697f2c9b130d3e8417ae82990d131e87360a3d
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-05-29 04:42:51 +0100

    Feature: added policy sections

--- src/Pages/Policy/__tests__/Policy.test.js
+++ src/Pages/Policy/__tests__/Policy.test.js
@@ -9,4 +9,16 @@ describe("Policy page", () => {
     const header = screen.getByText("My Policy");
     expect(header).toBeInTheDocument();
   });
+
+  it('should render policy sections', () => {
+    render(<Policy />);
+    const policyRef = screen.getByText("Policy reference")
+    const coverType = screen.getByText('Cover type')
+    const car = screen.getByText('Car')
+    const address = screen.getByText('Address')
+    expect(policyRef).toBeInTheDocument();
+    expect(coverType).toBeInTheDocument();
+    expect(car).toBeInTheDocument();
+    expect(address).toBeInTheDocument();
+  })
 });
--- src/Pages/Policy/index.js
+++ src/Pages/Policy/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Policy() {
   return (
-    <h2>My Policy</h2>
+    <>
+      <h2>My Policy</h2>
+      <div>Policy reference</div>
+      <div>Cover type</div>
+      <div>Car</div>
+      <div>Address</div>
+    </>
   )
 }
